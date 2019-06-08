d7ea35655095f169a4e556ecf83a2ace164f3452
WangYu <wangy2032.@163.com>
2019-06-09 01:03:24 +0800

    继续修改项目

--- StudentSystem/teacher/forms.py
+++ StudentSystem/teacher/forms.py
@@ -4,12 +4,21 @@ from flask_wtf import FlaskForm
 from wtforms import  StringField,PasswordField, DateTimeField, DateField,\
     SelectField, BooleanField, SubmitField, ValidationError, RadioField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from StudentSystem.models import Geren, db, Student, Xueji
 
 '''
 学生基本信息表单
 '''
 class StudentJiBenMsg(FlaskForm):
-    student_id = StringField('学号', validators=[DataRequired()])
+    def __init__(self):
+        super().__init__()
+        self.student_id.choices = [(student.student_id, student.student_id)for student in db.session.query(Student).all()]
+
+    def check_id(self, field):
+        tmp = Geren.query.filter_by(student_id=field.data).first()
+        if tmp:
+            raise ValidationError('该学生信息以录入')
+    student_id = SelectField('学号', validators=[check_id])
     name = StringField('姓名', validators=[DataRequired()])
     used_name = StringField('曾用名')
     p_status = SelectField('政治面貌',
@@ -27,15 +36,43 @@ class StudentJiBenMsg(FlaskForm):
     bir_address = StringField('出生地', validators=[DataRequired()])
     ad_time = StringField('入学时间', validators=[DataRequired()])
     ti_jiao = SubmitField('提交')
-    chong_zhi =SubmitField('重置')
-    modify = SubmitField('修改')
 
+'''
+学生基本信息修改表单
+'''
+class StudentJiBenMdifyMsg(FlaskForm):
+    student_id = StringField('学号')
+    name = StringField('姓名', validators=[DataRequired()])
+    used_name = StringField('曾用名')
+    p_status = SelectField('政治面貌',
+                           choices=[('党员', '党员'), ('共青团员', '共青团员'),('群众', '群众')],
+                           validators=[DataRequired()])
+    sex = SelectField('性别', choices=[('男', '男'), ('女', '女')],
+                      validators=[DataRequired()])
+    nation = StringField('民族', validators=[DataRequired()])
+    id_type = SelectField('证件类型', choices=[('二代身份证', '二代身份证')], validators=[DataRequired()])
+    id_number = StringField('证件号码', validators=[DataRequired()])
+    birth_time = StringField('出生时间', validators=[DataRequired()])
+    b_place = StringField('籍贯', validators=[DataRequired()])
+    acc_location = StringField('户口所在地', validators=[DataRequired()])
+    stu_source = StringField('生源地', validators=[DataRequired()])
+    bir_address = StringField('出生地', validators=[DataRequired()])
+    ad_time = StringField('入学时间', validators=[DataRequired()])
+    modify = SubmitField('修改')
 
 '''
 学生学籍信息表单
 '''
 class StudentXueJi(FlaskForm):
-    student_id = StringField('学号', validators=[DataRequired()])
+    def __init__(self):
+        super().__init__()
+        self.student_id.choices = [(student.student_id, student.student_id)for student in db.session.query(Student).all()]
+
+    def check_id(self, field):
+        tmp = Xueji.query.filter_by(student_id=field.data).first()
+        if tmp:
+            raise ValidationError('该学生信息以录入')
+    student_id = SelectField('学号', validators=[check_id])
     name = StringField('姓名', validators=[DataRequired()])
     school_year = StringField('学年', validators=[DataRequired()])
     semester = SelectField('学期', choices=[('第一学期', '第一学期'),
@@ -59,7 +96,34 @@ class StudentXueJi(FlaskForm):
     a_college = StringField('招生学院', validators=[DataRequired()])
     a_profession = StringField('招生专业', validators=[DataRequired()])
     ti_jiao = SubmitField('提交')
-    chong_zhi = SubmitField('重置')
+
+'''
+学生学籍信息修改表单
+'''
+class StudentXueJiModify(FlaskForm):
+    student_id = StringField('学号')
+    name = StringField('姓名', validators=[DataRequired()])
+    school_year = StringField('学年', validators=[DataRequired()])
+    semester = SelectField('学期', choices=[('第一学期', '第一学期'),
+                                          ('第二学期', '第二学期')])
+    grade = StringField('年级', validators=[DataRequired()])
+    college_name = StringField('学院名称', validators=[DataRequired()])
+    d_name = StringField('系名称', validators=[DataRequired()])
+    p_name = StringField('专业名称', validators=[DataRequired()])
+    class_name = StringField('班级名字', validators=[DataRequired()])
+    school_system = StringField('学制', validators=[DataRequired()])
+    xue_ji_zt = SelectField('学籍状态', choices=[('注册', '注册'),
+                                          ('没有注册', '没有注册')])
+    zai_xiao = SelectField('是否在校', choices=[('是', '是'),
+                                          ('否', '否')])
+    e_level = SelectField('学历层次', choices=[('专科', '专科'),
+                                          ('本科', '本科'),
+                                           ('硕士', '硕士'),
+                                           ('博士', '博士')])
+    t_method = StringField('培养方式', validators=[DataRequired()])
+    student_type = StringField('学生类型', validators=[DataRequired()])
+    a_college = StringField('招生学院', validators=[DataRequired()])
+    a_profession = StringField('招生专业', validators=[DataRequired()])
     modify = SubmitField('修改')
 
 '''
--- StudentSystem/teacher/views.py
+++ StudentSystem/teacher/views.py
@@ -4,8 +4,8 @@ from flask import render_template, flash, request, redirect, url_for
 from flask_login import login_required, current_user
 from StudentSystem import db
 from StudentSystem.models import Geren, Xueji, Student
-from StudentSystem.teacher.forms import StudentJiBenMsg, StudentXueJi, SearchForm
-from datetime import datetime
+from StudentSystem.teacher.forms import StudentJiBenMsg, StudentXueJi, \
+    SearchForm, StudentJiBenMdifyMsg, StudentXueJiModify
 
 
 """
@@ -45,44 +45,50 @@ def index():
             db.session.add(geren)
             db.session.commit()
             flash('添加成功！')
-        elif form.chong_zhi.data:
-            flash('重置成功！')
-            return redirect(url_for('teacher.index'))
     return render_template('teacher/add_student_jiben.html',search_form=search_form, form=form, **contxt)
 
-@teacher.route('/<student_id>/ji-ben-msg/show')
+@teacher.route('/<student_id>/ji-ben-msg/show', methods=['GET','POST'])
 @login_required
 def show(student_id):
     '''
     查看基本信息
     '''
     geren = Geren.query.filter_by(student_id=student_id).first()
-    form = StudentJiBenMsg()
+    form = StudentJiBenMdifyMsg()
+    if form.validate_on_submit():
+        if form.modify.data:
+            geren.student_id = form.student_id.data
+            geren.name = form.name.data
+            geren.user_name = form.used_name.data
+            geren.sex = form.sex.data
+            geren.id_type = form.id_type.data
+            geren.id_number = form.id_number.data
+            form.birth_time.data = geren.date_of_birth
+            geren.min_zu = form.nation.data
+            geren.p_status = form.p_status.data
+            geren.a_time = form.ad_time.data
+            geren.birthplace = form.b_place.data
+            geren.a_location = form.acc_location.data
+            geren.student_id = form.stu_source.data
+            geren.place_of_birth = form.bir_address.data
+            db.session.commit()
+            flash('更新成功')
+            return redirect(url_for('teacher.index'))
     form.student_id.data = geren.student_id
     form.name.data =geren.name
     form.used_name.data = geren.user_name
     form.sex.data = geren.sex
     form.id_type.data = geren.id_type
     form.id_number.data = geren.id_number
-    form.birth_time.data = datetime.strptime(geren.date_of_birth, '%Y-%m-%d')
+    form.birth_time.data = geren.date_of_birth
     form.nation.data = geren.min_zu
     form.p_status.data = geren.p_status
-    form.ad_time.data = datetime.strptime(geren.a_time, '%Y-%m-%d')
+    form.ad_time.data = geren.a_time
     form.b_place.data = geren.birthplace
     form.acc_location.data = geren.a_location
     form.stu_source.data = geren.student_id
     form.bir_address.data = geren.place_of_birth
     return render_template('teacher/show_geren_msg.html', form=form, geren=geren)
-
-
-'''
-录入学生成绩
-'''
-@teacher.route('/score/add')
-@login_required
-def add_score():
-    return  render_template('teacher/add_student_score.html')
-
 
 '''
 录入学生学籍
@@ -129,29 +135,58 @@ def add_xueji():
         return redirect(url_for('teacher.add_xueji'))
     return  render_template('teacher/add_student_xueji.html',search_form=search_form, form=form, **contxt)
 
-'''
-显示学籍信息
-'''
-@teacher.route('/<student_id>/xue-ji/show')
+@teacher.route('/<student_id>/xue-ji/show', methods=['GET','POST'])
 @login_required
 def show_xueji(student_id):
+    '''
+    显示学籍信息
+    '''
     student = Xueji.query.filter_by(student_id=student_id).first()
-    form = StudentXueJi()
-    form.name.data = student.name,
-    form.student_id.data = student.student_id,
-    form.school_year.data = student.school_year,
-    form.semester.data = student.semester,
-    form.grade.data = student.grade,
-    form.college_name.data = student.college_name,
-    form.d_name.data = student.d_name,
-    form.p_name.data = student.p_name,
-    form.class_name.data = student.class_name,
-    form.school_system.data = student.school_system,
-    form.xue_ji_zt.data = student.xue_ji_zt,
-    form.zai_xiao.data = student.zai_xiao,
-    form.e_level.data = student.e_level,
-    form.t_method.data = student.t_method,
-    form.student_type.data = student.student_type,
-    form.a_college.data = student.a_college,
+    form = StudentXueJiModify()
+    if form.validate_on_submit():
+        student.name = form.name.data
+        student.student_id = form.student_id.data
+        student.school_year = form.school_year.data
+        student.semester = form.semester.data
+        student.grade = form.grade.data
+        student.college_name = form.college_name.data
+        student.d_name = form.d_name.data
+        student.p_name = form.p_name.data
+        student.class_name = form.class_name.data
+        student.school_system = form.school_system.data
+        student.xue_ji_zt = form.xue_ji_zt.data
+        student.zai_xiao = form.zai_xiao.data
+        student.e_level = form.e_level.data
+        student.t_method = form.t_method.data
+        student.student_type = form.student_type.data
+        student.a_college = form.a_college.data
+        student.a_profession = form.a_profession.data
+        db.session.commit()
+        flash('更新成功')
+        return redirect(url_for('teacher.add_xueji'))
+    form.name.data = student.name
+    form.student_id.data = student.student_id
+    form.school_year.data = student.school_year
+    form.semester.data = student.semester
+    form.grade.data = student.grade
+    form.college_name.data = student.college_name
+    form.d_name.data = student.d_name
+    form.p_name.data = student.p_name
+    form.class_name.data = student.class_name
+    form.school_system.data = student.school_system
+    form.xue_ji_zt.data = student.xue_ji_zt
+    form.zai_xiao.data = student.zai_xiao
+    form.e_level.data = student.e_level
+    form.t_method.data = student.t_method
+    form.student_type.data = student.student_type
+    form.a_college.data = student.a_college
     form.a_profession.data = student.a_profession
     return render_template('teacher/show_xueji_msg.html', student=student, form=form)
+
+'''
+录入学生成绩
+'''
+@teacher.route('/score/add')
+@login_required
+def add_score():
+    return  render_template('teacher/add_student_score.html')
